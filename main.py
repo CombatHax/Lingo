78d798df98a8e94fbcd9a39c3261e5d526dd5927
CombatHax <92267016+CombatHax@users.noreply.github.com>
2022-01-26 14:02:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pyautogui as pag
-
-pag.type("yes")
-
+row = 1
+col = 1
+col_max = [10, 9, 7]
+while True:
+    if col > col_max[row - 1]:
+        row += 1
+        col = 1
+    if row > 3:
+        break
+    if col == 10:
+        pag.typewrite("110")
+    else:
+        pag.typewrite(str(row * 10 + col))
+    col += 1
+    pag.press('enter')
